da3c09a85749a67f4873c4340aeeeb0f908fa776
allansmithll <allanammancio2004@gmail.com>
2023-06-16 17:57:12 -0300

    Novas alterações nos links

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,15 +3,15 @@ import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa';
 import '../styles/components/socialnetworks.sass'
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn /> },
-    { name: "github", icon: <FaGithub />},
-    { name: "instagram", icon: <FaInstagram />}
+    { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/allan-alves-amancio-211632197/"},
+    { name: "github", icon: <FaGithub />, link: "https://github.com/AllanSmithll"},
+    { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/allan_smith_pb/"}
 ];
 
 const SocialNetworks = () => {
     return <section id="social-networks">
         {socialNetworks.map((network) => (
-            <a href="#" className='social-btn' id={network.name} key={network.name}>
+            <a href={network.link} className='social-btn' id={network.name} key={network.name}>
                 {network.icon}
             </a>
         ))}
