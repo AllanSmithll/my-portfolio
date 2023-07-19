340a8ef5219bdee36456b75046393b66e3b176bb
allansmithll <allanammancio2004@gmail.com>
2023-07-19 17:25:06 -0300

    Versão nova

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,6 +1,6 @@
 function getSemester() {
     const monthsPerSemester = 6;
-    const dateStart = new Date("03/14/2022");
+    const dateStart = new Date("02/08/2022");
     const currentDate = new Date();
     const totalMonths = (currentDate.getFullYear() - dateStart.getFullYear()) * 12 + (currentDate.getMonth() - dateStart.getMonth());
     const semester = Math.floor(totalMonths / monthsPerSemester) + 1;
@@ -12,7 +12,11 @@ const AboutContainer = () => {
     return (
         <section className="aboutcontainer">
             <h2>Sobre mim</h2>
-            <p>Sou o Allan Amâncio, sou estudante do {getSemester()}º período do Curso Superior de Tecnologia em Sistemas para Internet - IFPB. Atualmente trabalho com projetos fullstack, em diversas linguagens de programação e ferramentas utilitárias. Espero que goste de algum dos meus projetos. Fique à vontade!</p>
+            <p>Sou o Allan Amâncio, estudante do {getSemester()}º período do
+            Curso Superior de Tecnologia em Sistemas para Internet - IFPB.
+            Atualmente trabalho com projetos fullstack, em diversas linguagens
+            de programação e ferramentas utilitárias. Espero que goste de algum
+            dos meus projetos. Fique à vontade!</p>
         </section>
     );
 }
